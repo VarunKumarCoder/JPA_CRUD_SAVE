e9b6646f0ad98ee37b9305608ae7465ab22040f1
Meda Varun Kumar <codedotgo@gmail.com>
2024-12-10 12:41:17 +0530

    Added Required Annotations

--- JpaProj01-CRUDSaveMethod/src/main/java/com/cdc/entity/Doctor.java
+++ JpaProj01-CRUDSaveMethod/src/main/java/com/cdc/entity/Doctor.java
@@ -7,20 +7,25 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Table(name="JPA_DOCTOR_INFO")
 @Data
+@RequiredArgsConstructor
 public class Doctor {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer docId;
 	@Column(name="DOC_NAME", length=25)
+	@NonNull
 	private Integer docName;
+	@NonNull
 	@Column(name="SPECIALiZATION",length = 30)
 	private String specialiization;
-	
-	
-
+	@NonNull
+	@Column(name="INCOME")
+	private Double income;
 }
