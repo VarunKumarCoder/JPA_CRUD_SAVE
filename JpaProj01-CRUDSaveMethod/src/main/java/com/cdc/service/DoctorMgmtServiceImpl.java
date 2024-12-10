1819f9ad32e682544289303cfdfa46d77db14cbf
Meda Varun Kumar <codedotgo@gmail.com>
2024-12-10 12:56:23 +0530

    Service Implementation class created byu extending the service Interface

---
+++ JpaProj01-CRUDSaveMethod/src/main/java/com/cdc/service/DoctorMgmtServiceImpl.java
@@ -0,0 +1,13 @@
+package com.cdc.service;
+
+import com.cdc.entity.Doctor;
+
+public class DoctorMgmtServiceImpl implements IDoctorService {
+
+	@Override
+	public String registerDoctor(Doctor doctor) {
+		
+		return null;
+	}
+
+}
