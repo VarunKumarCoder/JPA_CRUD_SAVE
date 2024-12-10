a126ec477cea61de65693ec744aa51f5bbed60fe
Meda Varun Kumar <codedotgo@gmail.com>
2024-12-10 13:02:10 +0530

    business method created

--- JpaProj01-CRUDSaveMethod/src/main/java/com/cdc/service/DoctorMgmtServiceImpl.java
+++ JpaProj01-CRUDSaveMethod/src/main/java/com/cdc/service/DoctorMgmtServiceImpl.java
@@ -1,13 +1,18 @@
 package com.cdc.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.cdc.entity.Doctor;
+import com.cdc.repository.IDoctorRepo;
 
 public class DoctorMgmtServiceImpl implements IDoctorService {
-
+	
+	@Autowired
+	private IDoctorRepo doctorRepo;
 	@Override
 	public String registerDoctor(Doctor doctor) {
-		
-		return null;
+		Doctor doc=doctorRepo.save(doctor);
+		return "Doctor Id is saved with"+doc.getDocId();
 	}
 
 }
